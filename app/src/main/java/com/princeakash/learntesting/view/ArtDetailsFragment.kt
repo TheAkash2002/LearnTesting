3859af39fd6ff14732e6ce4ec7fdf58ab8c4af98
TheAkash2002 <mahapatraakash@gmail.com>
2021-09-02 08:02:51 +0530

    Completed Room setup

--- app/src/main/java/com/princeakash/learntesting/view/ArtDetailsFragment.kt
+++ app/src/main/java/com/princeakash/learntesting/view/ArtDetailsFragment.kt
@@ -2,11 +2,36 @@ package com.princeakash.learntesting.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.princeakash.learntesting.R
+import com.princeakash.learntesting.databinding.FragmentArtsDetailsBinding
 
 class ArtDetailsFragment: Fragment(R.layout.fragment_arts_details) {
+
+    private var fragmentBinding: FragmentArtsDetailsBinding? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtsDetailsBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.artImageView.setOnClickListener {
+            findNavController().navigate(ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment())
+        }
+
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    override fun onDestroyView() {
+        fragmentBinding = null
+        super.onDestroyView()
     }
 }
