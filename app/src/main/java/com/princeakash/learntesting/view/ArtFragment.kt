fc49b6c31f298ff0f437f2a18196219a20b2dc89
TheAkash2002 <mahapatraakash@gmail.com>
2021-09-02 07:49:53 +0530

    Completed ArtFragment with view binding

--- app/src/main/java/com/princeakash/learntesting/view/ArtFragment.kt
+++ app/src/main/java/com/princeakash/learntesting/view/ArtFragment.kt
@@ -3,10 +3,21 @@ package com.princeakash.learntesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.princeakash.learntesting.R
+import com.princeakash.learntesting.databinding.FragmentArtsBinding
 
 class ArtFragment: Fragment(R.layout.fragment_arts) {
+
+    private var fragmentBinding: FragmentArtsBinding? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentArtsBinding.bind(view)
+        fragmentBinding = binding
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
+        }
     }
 }
